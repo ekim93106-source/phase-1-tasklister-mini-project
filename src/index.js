4ec09a77efdc2d53fc7ff46906298b45dc19ae9e
kimani <ekim93106@gmail.com>
2026-01-02 16:51:39 +0300

    finished the tracklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    buildToDo(input.value);
+    form.reset();
+  });
 });
+
+function buildToDo(task) {
+  const ul = document.getElementById("tasks");
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  ul.appendChild(li);
+}
